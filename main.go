5da63729e9e915016795a2b332df6edfd6f078ca
Clarence Gui <55305469+Sanczzg@users.noreply.github.com>
2024-10-12 02:06:23 +0800

    Remove useless codes
    Need geosite.dat only with custom domain list

--- main.go
+++ main.go
@@ -15,9 +15,6 @@ var (
 	dataPath     = flag.String("datapath", filepath.Join("./", "data"), "Path to your custom 'data' directory")
 	datName      = flag.String("datname", "geosite.dat", "Name of the generated dat file")
 	outputPath   = flag.String("outputpath", "./publish", "Output path to the generated files")
-	exportLists  = flag.String("exportlists", "category-ads-all,tld-cn,cn,geolocation-cn,tld-!cn,geolocation-!cn,private,apple,icloud,google,steam", "Lists to be exported in plaintext format, separated by ',' comma")
-	excludeAttrs = flag.String("excludeattrs", "cn@!cn@ads,geolocation-cn@!cn@ads,geolocation-!cn@cn@ads", "Exclude rules with certain attributes in certain lists, seperated by ',' comma, support multiple attributes in one list. Example: geolocation-!cn@cn@ads,geolocation-cn@!cn")
-	toGFWList    = flag.String("togfwlist", "geolocation-!cn", "List to be exported in GFWList format")
 )
 
 func main() {
@@ -47,36 +44,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Process and split *excludeRules
-	excludeAttrsInFile := make(map[fileName]map[attribute]bool)
-	if *excludeAttrs != "" {
-		exFilenameAttrSlice := strings.Split(*excludeAttrs, ",")
-		for _, exFilenameAttr := range exFilenameAttrSlice {
-			exFilenameAttr = strings.TrimSpace(exFilenameAttr)
-			exFilenameAttrMap := strings.Split(exFilenameAttr, "@")
-			filename := fileName(strings.ToUpper(strings.TrimSpace(exFilenameAttrMap[0])))
-			excludeAttrsInFile[filename] = make(map[attribute]bool)
-			for _, attr := range exFilenameAttrMap[1:] {
-				attr = strings.TrimSpace(attr)
-				if len(attr) > 0 {
-					excludeAttrsInFile[filename][attribute(attr)] = true
-				}
-			}
-		}
-	}
-
-	// Process and split *exportLists
-	var exportListsSlice []string
-	if *exportLists != "" {
-		tempSlice := strings.Split(*exportLists, ",")
-		for _, exportList := range tempSlice {
-			exportList = strings.TrimSpace(exportList)
-			if len(exportList) > 0 {
-				exportListsSlice = append(exportListsSlice, exportList)
-			}
-		}
-	}
-
 	// Generate dlc.dat
 	if geositeList := listInfoMap.ToProto(excludeAttrsInFile); geositeList != nil {
 		protoBytes, err := proto.Marshal(geositeList)
@@ -96,38 +63,4 @@ func main() {
 		}
 	}
 
-	// Generate plaintext list files
-	if filePlainTextBytesMap, err := listInfoMap.ToPlainText(exportListsSlice); err == nil {
-		for filename, plaintextBytes := range filePlainTextBytesMap {
-			filename += ".txt"
-			if err := os.WriteFile(filepath.Join(*outputPath, filename), plaintextBytes, 0644); err != nil {
-				fmt.Println("Failed:", err)
-				os.Exit(1)
-			} else {
-				fmt.Printf("%s has been generated successfully in '%s'.\n", filename, *outputPath)
-			}
-		}
-	} else {
-		fmt.Println("Failed:", err)
-		os.Exit(1)
-	}
-
-	// Generate gfwlist.txt
-	if gfwlistBytes, err := listInfoMap.ToGFWList(*toGFWList); err == nil {
-		if f, err := os.OpenFile(filepath.Join(*outputPath, "gfwlist.txt"), os.O_RDWR|os.O_CREATE, 0644); err != nil {
-			fmt.Println("Failed:", err)
-			os.Exit(1)
-		} else {
-			encoder := base64.NewEncoder(base64.StdEncoding, f)
-			defer encoder.Close()
-			if _, err := encoder.Write(gfwlistBytes); err != nil {
-				fmt.Println("Failed:", err)
-				os.Exit(1)
-			}
-			fmt.Printf("gfwlist.txt has been generated successfully in '%s'.\n", *outputPath)
-		}
-	} else {
-		fmt.Println("Failed:", err)
-		os.Exit(1)
-	}
 }
